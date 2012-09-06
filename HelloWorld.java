659f38119269a0121f0f5cc90909e0e47a7cce0d
jrlilley <jon.r.lilley@gmail.com>
2012-09-06 13:28:05 +0100

    Signed-off-by: jrlilley <jon.r.lilley@gmail.com>

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,15 @@
 public class HelloWorld {
 	public static void main(String[] args) {
+		
+		int a;
+
 		System.out.println("Hello World!");
 		System.out.println("This is a silly test for git");
 		System.out.println("This is a new branch");
 		System.out.println("This is getting silly");
+
+		for (a=0;a<10;a++) {
+			System.out.println("Crazy man " + a);
+		}
 	}
 }
