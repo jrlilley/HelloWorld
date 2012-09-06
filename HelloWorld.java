1f0ca726dd7df503b7f4eb0e776da8e4b117f672
jrlilley <jon.r.lilley@gmail.com>
2012-09-06 13:03:01 +0100

    branch of a branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("This is a silly test for git");
 		System.out.println("This is a new branch");
+		System.out.println("This is a branch of branch1");
 	}
 }
